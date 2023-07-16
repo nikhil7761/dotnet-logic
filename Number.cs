706ce53c97a762111f594b82106435fd04a50370
Mr.NIKHIL VARGUDE <115720074+nikhil7761@users.noreply.github.com>
2023-07-16 20:10:32 +0530

    Add files via upload

---
+++ Number.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace primdfg{
+
+class Number{
+
+//#region we are search for prime numbers
+public static void Main(string[]args)
+{
+
+Console.WriteLine("please enter the max no");
+
+int nmaxnumber=Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("Prime number between {0} and {1} are:",2,nmaxnumber);
+
+bool isPrimeNumber=true;
+
+//this loop for checking maxnumber
+for(int i=2;i<nmaxnumber;i++)
+{
+
+  for(int j=2;j<nmaxnumber;j++){
+
+    if(i!=j && i%j==0)
+    {
+      isPrimeNumber=false;
+      break;
+    }
+
+    if(isPrimeNumber){
+        Console.WriteLine(i);
+    }
+     
+   //  isPrimeNumber=true;
+
+  }
+    
+}
+
+Console.ReadLine();
+
+}
+
+}
+
+}
---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿namespace unity{
+
+
+class Future
+{
+
+public virtual int Add(int x, int y)
+{
+    int q=x+y;
+    return q ;
+}
+
+public virtual int Add(int x,int y,int z){
+    return x+y+z;
+}
+
+
+static void Main(String[]args)
+{
+
+Future a=new Future();
+int result=a.Add(12,34);
+
+int result2=a.Add(23,12,12);
+
+
+Console.WriteLine("Addition of 2 number"+result);
+
+Console.WriteLine("addition of 3 number"+result2);
+
+
+}
+
+
+
+}
+// class run:Future{
+//         public override int Add(int x, int y)
+//         {
+//             return base.Add(x, y);
+//         }
+
+}
